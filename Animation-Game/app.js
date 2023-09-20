2f90dd4a60366016d00214db974e5f1fa288f124
Ahmed <ahmed.raza1054@gmail.com>
2023-09-21 02:55:30 +0500

    one side character completed
    one side character completed

--- Animation-Game/app.js
+++ Animation-Game/app.js
@@ -1,11 +1,52 @@
-function moveForward(e) {
-  var img = document.getElementById("charactor");
-  img.value = e.src = "animation-img/chun-li/move-forward.gif";
+var move = 0;
+
+function animation() {
+  var charactor = document.getElementById("charactor");
+  if (event.keyCode === 68) {
+    move = move + 10;
+    charactor.style.left = move + "px";
+    charactor.src = "animation-img/chun-li/move-forward.gif";
+  }
+  if (event.keyCode === 65) {
+    move = move - 10;
+    charactor.style.left = move + "px";
+    charactor.src = "animation-img/chun-li/move-backward.gif";
+  }
+  if (event.keyCode === 87) {
+    charactor.src = "animation-img/chun-li/flaying-kick.gif";
+    charactor.style.height = "450px";
+    setTimeout(function () {
+      charactor.src = "animation-img/chun-li/standing.gif";
+      charactor.style.height = "330px";
+    }, 2500);
+  }
+  if (event.keyCode === 83) {
+    charactor.src = "animation-img/chun-li/forward-flip-kick.gif";
+    charactor.style.height = "450px";
+
+    setTimeout(function () {
+      charactor.src = "animation-img/chun-li/standing.gif";
+      charactor.style.height = "330px";
+    }, 2100);
+  }
+  if (event.keyCode === 32) {
+    charactor.src = "animation-img/chun-li/standing-360-kick.gif";
+    charactor.style.height = "450px";
+
+    setTimeout(function () {
+      charactor.src = "animation-img/chun-li/standing.gif";
+      charactor.style.height = "330px";
+    }, 2000);
+  }
+  if (event.keyCode === 17) {
+    charactor.src = "animation-img/chun-li/sitting-slap.gif";
+    charactor.style.height = "450px";
+
+    setTimeout(function () {
+      charactor.src = "animation-img/chun-li/standing.gif";
+      charactor.style.height = "330px";
+    }, 1000);
+  }
 }
 
-function moving(e) {
-  document.getElementById("charactor").value = e.src =
-    "animation-img/chun-li/standing.gif";
-  e.style.animationName = "roll";
-  console.log("working");
-}
+window.onkeydown = animation;
