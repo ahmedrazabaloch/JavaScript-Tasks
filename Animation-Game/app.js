20c6a64292c4b23a1d3a4fc3caee32dc1e4b1170
Ahmed <ahmed.raza1054@gmail.com>
2023-09-21 16:03:01 +0500

    animation game one side ready
    animation game one side ready

--- Animation-Game/app.js
+++ Animation-Game/app.js
@@ -2,50 +2,50 @@ var move = 0;
 
 function animation() {
   var charactor = document.getElementById("charactor");
-  if (event.keyCode === 68 && move < 1300) {
+  if (event.keyCode === 68 && move < 1300) { // 68==> D 
     move = move + 10;
     charactor.style.left = move + "px";
-    charactor.src = "animation-img/chun-li/move-forward.gif";
+    charactor.src = "animation-img/Sol-badguy/forwardMove.gif";
   }
-  if (event.keyCode === 65 && move > 10) {
+  if (event.keyCode === 65 && move > 10) { // 65 ===> A
     move = move - 10;
     charactor.style.left = move + "px";
-    charactor.src = "animation-img/chun-li/move-backward.gif";
+    charactor.src = "animation-img/Sol-badguy/backwardMove.gif";
   }
-  if (event.keyCode === 87) {
-    charactor.src = "animation-img/chun-li/flaying-kick.gif";
+  if (event.keyCode === 87) { // 87 ===> W
+    charactor.src = "animation-img/Sol-badguy/power-punch.gif";
     charactor.style.height = "450px";
     setTimeout(function () {
-      charactor.src = "animation-img/chun-li/standing.gif";
+      charactor.src = "animation-img/Sol-badguy/action.gif";
       charactor.style.height = "330px";
-    }, 2500);
+    }, 4000);
   }
-  if (event.keyCode === 83) {
-    charactor.src = "animation-img/chun-li/forward-flip-kick.gif";
+  if (event.keyCode === 83) { // 83 ===> S
+    charactor.src = "animation-img/Sol-badguy/flip kick.gif";
     charactor.style.height = "450px";
 
-    setTimeout(function () {
-      charactor.src = "animation-img/chun-li/standing.gif";
+    setTimeout(function () { 
+      charactor.src = "animation-img/Sol-badguy/action.gif";
       charactor.style.height = "330px";
-    }, 2100);
+    }, 1300);
   }
-  if (event.keyCode === 32) {
-    charactor.src = "animation-img/chun-li/forward-punch.gif";
+  if (event.keyCode === 32) { // 32 ===> SPACE
+    charactor.src = "animation-img/Sol-badguy/jump.gif";
     charactor.style.height = "450px";
 
-    setTimeout(function () {
-      charactor.src = "animation-img/chun-li/standing.gif";
+    setTimeout(function () { 
+      charactor.src = "animation-img/Sol-badguy/action.gif";
       charactor.style.height = "330px";
     }, 1200);
   }
-  if (event.keyCode === 17) {
-    charactor.src = "animation-img/chun-li/sitting-slap.gif";
+  if (event.keyCode === 17) { // 17 ===> CTRL
+    charactor.src = "animation-img/Sol-badguy/power-plus.gif";
     charactor.style.height = "400px";
 
     setTimeout(function () {
-      charactor.src = "animation-img/chun-li/standing.gif";
+      charactor.src = "animation-img/Sol-badguy/action.gif";
       charactor.style.height = "330px";
-    }, 1000);
+    }, 3000);
   }
 }
 
